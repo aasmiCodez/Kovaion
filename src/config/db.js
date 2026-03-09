4bce57d779bb72738c7f61203046a9c9c36dda35
Aasmi C <aasmideveloper@gmail.com>
2026-03-09 12:48:44 +0530

    Delete src directory

--- src/config/db.js
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose";
-import { env } from "./env.js";
-
-export async function connectDb() {
-  mongoose.set("sanitizeFilter", true);
-  await mongoose.connect(env.mongoUri);
-}
